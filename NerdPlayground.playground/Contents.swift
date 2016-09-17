38e3f53e7d2b77c0c3607ae99653c3e9880098d1
Glenn Wiley <glennwiley@gmail.com>
2016-09-17 14:58:22 -0500

    more samples

--- NerdPlayground.playground/Contents.swift
+++ NerdPlayground.playground/Contents.swift
@@ -1,4 +1,24 @@
 
 import UIKit
 
-var str = "Hello, playground"
+var countingUp : [Int]
+countingUp = [1,2,3,4,5]
+
+for i in 0 ..< countingUp.count {
+    let string = countingUp[i]
+    // use 'string'
+}
+
+for i in countingUp {
+    // use 'string'
+}
+
+for (i, string) in countingUp.enumerated() {
+    // blah
+}
+
+var airports: [String: String] = ["YYZ": "Toronto Pearson", "DUB": "Dublin"]
+
+for (airportCode, airportName) in airports {
+    print("AirportCode \(airportCode) - AirportName \(airportName)")
+}
